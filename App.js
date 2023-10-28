23c99b0a5eaafffb9eff810c9000aa58e3ebd47f
Umar Farooq-Ansari <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>
2023-10-28 08:36:30 +0500

    'firstProjct'

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> Hello World Umar 
+        is coming  95177</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
+  fontSize:'bold',
   },
 });
